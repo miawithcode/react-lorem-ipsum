2f029cfb9239c52c28037ae36f38c4440ec0016b
miawithcode <cuttontail@gmail.com>
2024-01-06 13:32:36 +0800

    feat: add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 const App = () => {
   return (
-    <div>App</div>
+    <section>
+      <h4>Tired of boring lorem ipsum?</h4>
+      <form>
+        <label htmlFor="amount">Paragraphs: </label>
+        <input type="number" id="amount" name="amount" min="1" step="1" max="8" />
+        <button type="button">Generate</button>
+      </form>
+    </section>
   )
 }
 export default App
