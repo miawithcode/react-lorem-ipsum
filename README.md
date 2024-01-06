a9902528cc49e2d6fcb95c7461a82fce6d668522
miawithcode <cuttontail@gmail.com>
2024-01-06 14:21:22 +0800

    feat: add generate text functionality

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # Lorem Ipsum
 
 Inspiration of this project - [Hipster Ipsum](https://hipsum.co/)
+
+## Nanoid Library
+
+use Nanoid to generate unique ID in this project.
+
+1. Install
+   ```sh
+   npm i nanoid
+   ```
+2. Usage
+   ```jsx
+   import { nanoid } from 'nanoid';
+
+   const id = nanoid(); // Generates a 21-character random ID
+
+   console.log(id); // Output: "Uakgb_J5m9g-0JDMbcJqL"
+   ```
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,40 @@
+import { useState } from 'react';
+import data from './data';
+import { nanoid } from 'nanoid';
+
 const App = () => {
+  const [count, setCount] = useState(1);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    setText(data.slice(0, amount));
+  };
+
   return (
     <section>
       <h4>Tired of boring lorem ipsum?</h4>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="amount">Paragraphs: </label>
-        <input type="number" id="amount" name="amount" min="1" step="1" max="8" />
-        <button type="button">Generate</button>
+        <input
+          type="number"
+          id="amount"
+          name="amount"
+          min="1"
+          step="1"
+          max="8"
+          value={count}
+          onChange={(e) => setCount(e.target.value)}
+        />
+        <button type="submit">Generate</button>
       </form>
+      <article>
+        {text.map((item) => {
+          return <p key={nanoid()}>{item}</p>;
+        })}
+      </article>
     </section>
-  )
-}
-export default App
+  );
+};
+export default App;
